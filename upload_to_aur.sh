6a38bc7f6086745767c3c0b639e8180bea0b1355
chun-awa <110163919+chun-awa@users.noreply.github.com>
2024-10-21 20:33:18 +0800

    Add upload_to_aur.sh

---
+++ upload_to_aur.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+tempdir=$(mktemp -d)
+
+for pkgbuild in */PKGBUILD; do
+    pkgname=$(basename "$(dirname "$pkgbuild")")
+    version=$(grep -oP '^pkgver=\K.*' "$pkgbuild")
+    git clone -c init.defaultBranch=master "ssh://aur@aur.archlinux.org/${pkgname}.git" "${tempdir}/${pkgname}"
+    (cd $pkgname && cp PKGBUILD .SRCINFO "${tempdir}/${pkgname}")
+    (cd "${tempdir}/${pkgname}" && git commit -am "Update version to ${version}" && git push)
+    echo $pkgname
+done
+
+rm -rf "${tempdir}"
