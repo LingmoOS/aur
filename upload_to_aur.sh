a13752eba23cac522da1e536d1620517340a02a4
chun-awa <110163919+chun-awa@users.noreply.github.com>
2025-03-03 20:19:39 +0800

    fix(upload_to_aur): checkout master branch before pushing

--- upload_to_aur.sh
+++ upload_to_aur.sh
@@ -6,6 +6,7 @@ for pkgbuild in */PKGBUILD; do
     pkgname=$(basename "$(dirname "$pkgbuild")")
     version=$(grep -oP '^pkgver=\K.*' "$pkgbuild")
     git clone -c init.defaultBranch=master "ssh://aur@aur.archlinux.org/${pkgname}.git" "${tempdir}/${pkgname}"
+    (cd "${tempdir}/${pkgname}" && git checkout master)
     (cd $pkgname && cp PKGBUILD .SRCINFO "${tempdir}/${pkgname}")
     (cd "${tempdir}/${pkgname}" && git commit -am "Update version to ${version}" && git push)
     echo $pkgname
